0a6a3a004b3939051639fc19558b59ac0547fcb4
Andam Mosa <andam.mosa@hotmail.com>
2024-07-30 20:00:27 -0400

    Fixed the audio reset audio button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function App() {
   const [audioTime, setAudioTime] = useState(0);
+  const audioRef = useRef(null);
 
   const handleResetAudio = () => {
     setAudioTime(5);
+    audioRef.current.currentTime = audioTime;
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello, React!</h1>
-        <audio controls>
+        <audio controls ref={audioRef}>
           <source src="audio/mahmoud-khalil-al-husary-surah-001.mp3" type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
